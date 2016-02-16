84067c31bc5508fe312bed4f04074aec3ac70cab
Robert Redl <robert.redl@lmu.de>
2016-02-16 15:30:06 +0100

    added support for multiple video files

--- init.rb
+++ init.rb
@@ -5,34 +5,48 @@ Redmine::Plugin.register :redmine_videojs do
     author 'Robert Redl'
     description 'Embed videos into wiki pages using the HTML5 Video.js player.'
     url 'https://github.com/r5r3/redmine_videojs_pluigin'
-    version '0.0.1'
+    version '0.0.2'
 end
 
 # wiki macro for embedding videos
 Redmine::WikiFormatting::Macros.register do
     macro :video do |o, args|
-        width = args[1].gsub(/\D/,'') if args[1]
-        height = args[2].gsub(/\D/,'') if args[2]
-        width ||= "auto"
-        height ||= "auto"
+        # counter for video index
         @num ||= 0
         @num = @num + 1
-        attachment = o.attachments.find_by_filename(args[0]) if o.respond_to?('attachments')
-
-        if attachment
-            file_url = url_for(:only_path => false, :controller => 'attachments', :action => 'download', :id => attachment, :filename => attachment.filename)
-        else
-            file_url = args[0].gsub(/<.*?>/, '').gsub(/&lt;.*&gt;/,'')
+        # find width and height
+        for arg in args
+            if arg.starts_with?("width=")
+                width = arg[6..-1]
+            end
+            if arg.starts_with?("height=")
+                width = arg[7..-1]
+            end
         end
-out = <<END
-<div>
-<video id="video_#{@num}" class="video-js vjs-default-skin" controls preload="auto" width="#{width}" height="#{height}" data-setup='{}'>
-  <source src="#{file_url}" type="video/mp4">
-</video>
-<small>To view this video please enable JavaScript, and consider upgrading to a web browser
-that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>.</small>
-</div>
-END
+        # default is automatic
+        width ||= "auto"
+        height ||= "auto"
+        # start html block for video
+        out = "<div>\n"
+        out += "<video id=\"video_#{@num}\" class=\"video-js vjs-default-skin\" controls preload=\"auto\" width=\"#{width}\" height=\"#{height}\" data-setup=\"{}\">\n"
+        # find files in arguments
+        accepted_formats = [".mp4", ".webm"]
+        for arg in args
+            if accepted_formats.include? File.extname(arg)
+                attachment = o.attachments.find_by_filename(arg) if o.respond_to?('attachments')
+                if attachment
+                    file_url = url_for(:only_path => false, :controller => 'attachments', :action => 'download', :id => attachment, :filename => attachment.filename)
+                else
+                    file_url = arg.gsub(/<.*?>/, '').gsub(/&lt;.*&gt;/,'')
+                end
+                file_format = File.extname(arg)
+                out += "<source src=\"#{file_url}\" type=\"video/#{file_format[1..-1]}\">\n"
+            end
+        end
+        # finalize video html block
+        out += "<p class=\"vjs-no-js\">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href=\"http://videojs.com/html5-video-support/\" target=\"_blank\">supports HTML5 video</a></p>\n"
+        out += "</video>\n"
+        out += "</div>\n"
         out.html_safe
     end
 end
@@ -42,6 +56,7 @@ module RedmineVideojsPlugin
         # include video.js css and JavaScript files into the header
         def view_layouts_base_html_head(context={})
             tags = [stylesheet_link_tag('https://vjs.zencdn.net/5.6.0/video-js.css')]
+            tags << javascript_include_tag('https://vjs.zencdn.net/ie8/1.1.1/videojs-ie8.min.js')
             tags << javascript_include_tag('https://vjs.zencdn.net/5.6.0/video.min.js')
             return tags.join(' ')
         end
