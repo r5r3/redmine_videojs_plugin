2936ce3d84548f9fe83596f55e7d5fd97b995368
Robert Redl <robert.redl@lmu.de>
2016-02-23 08:49:34 +0100

    updated videojs version

--- init.rb
+++ init.rb
@@ -20,7 +20,7 @@ Redmine::WikiFormatting::Macros.register do
                 width = arg[6..-1]
             end
             if arg.starts_with?("height=")
-                width = arg[7..-1]
+                height = arg[7..-1]
             end
         end
         # default is automatic
@@ -35,7 +35,7 @@ Redmine::WikiFormatting::Macros.register do
             if accepted_formats.include? File.extname(arg)
                 attachment = o.attachments.find_by_filename(arg) if o.respond_to?('attachments')
                 if attachment
-                    file_url = url_for(:only_path => false, :controller => 'attachments', :action => 'download', :id => attachment, :filename => attachment.filename)
+                    file_url = url_for(:only_path => true, :controller => 'attachments', :action => 'download', :id => attachment, :filename => attachment.filename)
                 else
                     file_url = arg.gsub(/<.*?>/, '').gsub(/&lt;.*&gt;/,'')
                 end
@@ -46,6 +46,9 @@ Redmine::WikiFormatting::Macros.register do
         # finalize video html block
         out += "<p class=\"vjs-no-js\">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href=\"http://videojs.com/html5-video-support/\" target=\"_blank\">supports HTML5 video</a></p>\n"
         out += "</video>\n"
+        if @num == 1
+            out += "<script src=\"https://vjs.zencdn.net/5.7.1/video.js\"></script>\n"
+        end if
         out += "</div>\n"
         out.html_safe
     end
@@ -55,10 +58,9 @@ module RedmineVideojsPlugin
     class Hooks < Redmine::Hook::ViewListener
         # include video.js css and JavaScript files into the header
         def view_layouts_base_html_head(context={})
-            tags = [stylesheet_link_tag('https://vjs.zencdn.net/5.6.0/video-js.css')]
-            tags << javascript_include_tag('https://vjs.zencdn.net/ie8/1.1.1/videojs-ie8.min.js')
-            tags << javascript_include_tag('https://vjs.zencdn.net/5.6.0/video.min.js')
-            return tags.join(' ')
+            tags = [stylesheet_link_tag("https://vjs.zencdn.net/5.7.1/video-js.css")]
+            tags << javascript_include_tag("https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js")
+            return tags.join("\n")
         end
     end
 end
